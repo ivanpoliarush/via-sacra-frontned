7a59c56c5a8cd9b56bde9015fa01cb699f182530
Ivan Poliarush <sashapolarush@gmail.com>
2024-10-12 18:08:41 +0300

    feat: make info section responsive

--- src/features/home-page/ui/main/main.tsx
+++ src/features/home-page/ui/main/main.tsx
@@ -1,10 +1,11 @@
+import { InfoSection } from '../info-section/info-section';
 import { MainSection } from '../main-section/main-section';
 
 export const HomePage = ({ lang }: { lang: string }) => {
 	return (
 		<main>
 			<MainSection lang={lang} />
-			{/* <InfoSection lang={lang} /> */}
+			<InfoSection lang={lang} />
 			{/* <ServicesSection lang={lang} /> */}
 			{/* <WorkProgressSection lang={lang} /> */}
 			{/* <ReviewsSection lang={lang} /> */}
