ecc8219b700990e5487d86fd08ca2fe0c4921cbc
Ivan Poliarush <sashapolarush@gmail.com>
2024-10-14 18:47:32 +0300

    feat: change login guard

--- src/features/admin-page/ui/components/login-guard/login-guard.tsx
+++ src/features/admin-page/ui/components/login-guard/login-guard.tsx
@@ -1,18 +1,21 @@
 'use client';
 
 import { useAdminUser } from '@/features/admin-page/hooks/use-admin-user';
-import { redirect } from 'next/navigation';
+import { redirect, usePathname, useSearchParams } from 'next/navigation';
 import { AdminLoginGuardProps } from './login-guard.props';
 
 export const AdminLoginGuard = ({ isAuth, children }: AdminLoginGuardProps) => {
 	const { isAuthenticated } = useAdminUser();
+	const pathname = usePathname();
+	const params = useSearchParams();
 
 	if (!isAuthenticated && isAuth) {
-		return redirect('/admin');
+		return redirect(`/admin?redirect=${pathname}`);
 	}
 
 	if (isAuthenticated && !isAuth) {
-		return redirect('/admin/orders');
+		const redirectUrl = params.get('redirect') || '/admin/orders';
+		return redirect(redirectUrl);
 	}
 
 	return <>{children}</>;
